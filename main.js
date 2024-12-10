f23e51e2ea7b33926be65a220baed62b1cd50806
vargha1 <vargha.rahavi@gmail.com>
2024-12-10 11:53:00 +0330

    VR

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ import '/style.css'
 let hint = true
 $('#magazine').turn({ gradients: true, acceleration: true, when: { turning: () => { hint = false } } });
 
+const regex = /\?(\d+)$/; // Captures the number after the question mark
+const match = window.location.href.match(regex);
+$('#magazine').turn("page", match ? match[1] : 1)
+
 setInterval(() => {
   if (hint == true) {
     $('#magazine').turn("peel", "tr");
